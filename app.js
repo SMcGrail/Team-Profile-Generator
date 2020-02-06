ba1f02d0654da7bceaf562908df39bc7fd9cca82
Cake <mcgrail.shauna@gmail.com>
2020-02-05 21:17:42 -0700

    added all files and directories

---
+++ app.js
@@ -0,0 +1,10 @@
+const inquirer = require('inquirer');
+const fs = require ('fs');
+
+const Engineer = require("./js/engineer");
+
+const Employee = require("./js/employee");
+
+const Intern = require("./js/intern");
+
+const Manager = require("./js/manager")
